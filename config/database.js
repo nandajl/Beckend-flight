2110549b247a6c18c562820a49460b8d4547fb8c
nandajl <nandajelubis@gmail.com>
2022-11-28 14:25:00 +0700

    [Nanda] configure database for deploy

--- config/database.js
+++ config/database.js
@@ -1,32 +1,36 @@
 
 /** Destruct environment variable to get database configuration */
 const {
-  DB_USERNAME = "postgres",
+  DB_USER = "postgres",
   DB_PASSWORD = "12345",
   DB_HOST = "127.0.0.1",
   DB_NAME = "takeoff_api",
+  DB_PORT = "5432"
 } = process.env;
 
 module.exports = {
   development: {
-    username: DB_USERNAME,
+    username: DB_USER,
     password: DB_PASSWORD,
     database: `${DB_NAME}`,
     host: DB_HOST,
-    dialect: "postgres",
+    port: DB_PORT,
+    dialect: "postgres"
   },
   test: {
-    username: DB_USERNAME,
+    username: DB_USER,
     password: DB_PASSWORD,
     database: `${DB_NAME}`,
     host: DB_HOST,
-    dialect: "postgres",
+    port: DB_PORT,
+    dialect: "postgres"
   },
   production: {
-    username: DB_USERNAME,
+    username: DB_USER,
     password: DB_PASSWORD,
     database: `${DB_NAME}`,
     host: DB_HOST,
-    dialect: "postgres",
-  },
+    port: DB_PORT,
+    dialect: "postgres"
+  }
 };
